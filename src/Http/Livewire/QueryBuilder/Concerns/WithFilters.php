fb5e69e065dd0bc2d8cee18e19edeacd67afaf04
Simon Barrett <simonbarrett@acttraining.org.uk>
2023-10-19 16:43:01 +0200

    :sparkles: add filters

--- src/Http/Livewire/QueryBuilder/Concerns/WithFilters.php
+++ src/Http/Livewire/QueryBuilder/Concerns/WithFilters.php
@@ -4,6 +4,7 @@ namespace ACTTraining\QueryBuilder\Http\Livewire\QueryBuilder\Concerns;
 
 use ACTTraining\QueryBuilder\Http\Livewire\QueryBuilder\Filters\BooleanFilter;
 use ACTTraining\QueryBuilder\Http\Livewire\QueryBuilder\Filters\DateFilter;
+use ACTTraining\QueryBuilder\Http\Livewire\QueryBuilder\Filters\SelectFilter;
 use ACTTraining\QueryBuilder\Http\Livewire\QueryBuilder\Filters\TextFilter;
 
 trait WithFilters
@@ -28,13 +29,24 @@ trait WithFilters
     {
         return [
             TextFilter::make('Name', 'full_name')->useOperator('like'),
+            SelectFilter::make('Location', 'contract.location.name')->withOptions([
+                'ACT Skills Centre, Caerphilly' => 'ACT Skills Centre, Caerphilly',
+                'Home Based' => 'Home Based',
+            ]),
             BooleanFilter::make('Line Manager', 'contract.line_manager'),
-            DateFilter::make('Start Date', 'contract.start_date')->useOperator('<='),
+            DateFilter::make('Started on or after', 'contract.start_date')->useOperator('>='),
+            DateFilter::make('Started on or before', 'contract.start_date')->useOperator('<='),
         ];
     }
 
     public function resetFilters(): void
     {
         $this->filterValues = [];
+        $this->resetPage();
+    }
+
+    public function updatedFilterValues(): void
+    {
+        $this->resetPage();
     }
 }
--- src/Http/Livewire/QueryBuilder/Filters/SelectFilter.php
+++ src/Http/Livewire/QueryBuilder/Filters/SelectFilter.php
@@ -4,4 +4,19 @@ namespace ACTTraining\QueryBuilder\Http\Livewire\QueryBuilder\Filters;
 
 class SelectFilter extends BaseFilter
 {
+    public $options = [];
+
+    public string $component = 'select';
+
+    public function options()
+    {
+        return $this->options;
+    }
+
+    public function withOptions(array $options = []): static
+    {
+        $this->options = $options;
+
+        return $this;
+    }
 }
