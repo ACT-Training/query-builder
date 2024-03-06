099bf134f072cc90366be3534741b60a84d3f56b
Simon Barrett <simonbarrett@acttraining.org.uk>
2024-03-06 13:03:49 +0000

    :bug: fix #47

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -5,8 +5,8 @@
             <x-tabler-search class="w-4 h-4 text-gray-500 dark:text-gray-400"/>
         </div>
         <input wire:model.live.debounce.500ms="searchBy" type="text" id="simple-search" class="bg-white border border-gray-300 text-gray-700 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 shadow" placeholder="Search..." >
-        <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-            <x-tabler-trash wire:click="resetSearch" class="w-4 h-4 text-gray-400 hover:text-red-400 dark:text-gray-400"/>
+        <div class="absolute inset-y-0 right-0 flex items-center pr-3">
+            <x-tabler-trash wire:click.live="resetSearch" class="w-4 h-4 text-gray-400 hover:text-red-400 dark:text-gray-400 cursor-pointer"/>
         </div>
     </div>
 </div>
