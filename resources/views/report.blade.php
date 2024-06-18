642ef3b9082f1a83aabc10215da284e0dfe67830
Simon Barrett <simonbarrett@acttraining.org.uk>
2024-06-18 14:38:47 +0100

    :sparkles: add scroll to table and report

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -45,7 +45,7 @@
 
         @if($this->rows->count())
 
-            <div class="relative">
+            <div class="relative overflow-x-auto">
                 <table class="w-full text-sm text-left text-gray-500">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                     <tr class="border-y border-gray-200">
