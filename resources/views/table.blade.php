684fa3cfc77fc9bfe2e9f43a32fa8a21e83155c8
Simon Barrett <simonbarrett@acttraining.org.uk>
2024-07-15 13:48:06 +0100

    style change

--- resources/views/table.blade.php
+++ resources/views/table.blade.php
@@ -23,7 +23,7 @@
 
         <div>
             @if($this->rows->count())
-                <div class="relative overflow-x-auto overflow-y-visible">
+                <div class="relative overflow-x-auto overflow-y-auto">
                     <table class="w-full text-sm text-left text-gray-500" wire:key="table-1">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50">
                         <tr class="border-y border-gray-200">
