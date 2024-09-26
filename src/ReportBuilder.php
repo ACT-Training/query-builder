8261b06af175e46e7f275a92f1ba71f5267feb31
Simon Barrett <simonbarrett@acttraining.org.uk>
2024-09-26 09:32:38 +0100

    :construction: Work in progress

--- src/ReportBuilder.php
+++ src/ReportBuilder.php
@@ -6,7 +6,7 @@ namespace ACTTraining\QueryBuilder;
 
 use ACTTraining\QueryBuilder\Support\Concerns\WithColumnsBuilder;
 
-abstract class ReportBuilder extends TableBuilder
+abstract class ReportBuilder extends QueryBuilder
 {
     use WithColumnsBuilder;
 
