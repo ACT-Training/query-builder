7630ddfec665c2697a2d35d11c9faa4dd6d5adbc
Simon Barrett <simonbarrett@acttraining.org.uk>
2024-10-02 11:51:36 +0100

    :construction: Work in progress

--- resources/views/report-editor.blade.php
+++ resources/views/report-editor.blade.php
@@ -78,7 +78,7 @@
                     wire:key="export-button"
                     wire:click="exportReportBuilder"
             >
-                <x-tabler-table-export class="p-2 text-gray-600 hover:bg-gray-100 rounded"/>
+                <x-tabler-table-export class="p-2 w-6 h-6 text-gray-600 hover:bg-gray-100 rounded"/>
             </button>
 
             <button
