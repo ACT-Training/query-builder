cfc62f485726aa53aef9d4683e4d214d491ea04c
Simon Barrett <simonbarrett@acttraining.org.uk>
2024-12-20 10:38:57 +0000

    :bug: prevent auto complete in filters

--- resources/views/filters.blade.php
+++ resources/views/filters.blade.php
@@ -6,11 +6,11 @@
     </button>
 
     <div x-show="show" class="m-2 mb-4 bg-white rounded shadow" x-transition>
-        <div class="p-4 px-6 grid grid-cols-3 gap-6 text-sm">
+        <form class="p-4 px-6 grid grid-cols-3 gap-6 text-sm" autocomplete="off">
             @foreach($this->filters() as $filter)
                 @include('query-builder::filters.' . $filter->component())
             @endforeach
-        </div>
+        </form>
         <div class="p-2 flex items-center justify-between gap-2 bg-gray-50 text-sm rounded-b border-t border-t-gray-200">
 
             <button @click="show = false" class="flex items-center gap-2 text-gray-500 hover:text-blue-500 text-sm">
