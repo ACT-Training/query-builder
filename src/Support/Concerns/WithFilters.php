71637e13228f90e4f80a1541a978467f925b745d
Simon Barrett <simonbarrett@acttraining.org.uk>
2025-03-03 11:36:53 +0000

    :construction: Work in progress

--- src/Support/Concerns/WithFilters.php
+++ src/Support/Concerns/WithFilters.php
@@ -2,9 +2,6 @@
 
 namespace ACTTraining\QueryBuilder\Support\Concerns;
 
-use Livewire\Attributes\Session;
-use Livewire\Attributes\Url;
-
 trait WithFilters
 {
     protected bool $displayFilters = false;
